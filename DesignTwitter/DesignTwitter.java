630ba811f7687dd2a459f6681a91c9c6811d5ee6
Xingyu Liu <lxy.meteor@gmail.com>
2017-10-03 16:55:55 -0400

    finish twitter code

--- DesignTwitter/DesignTwitter.java
+++ DesignTwitter/DesignTwitter.java
@@ -13,8 +13,8 @@
 
 
 public class MiniTwitter {
-    Map<Integer, List<Integer>> friendship;
-    Map<Integer, List<Tweet>> tweets;
+    Map<Integer, List<Integer>> friendship; // can't be modified other than follow and unfollow
+    Map<Integer, List<Tweet>> tweets;  // save tweets with its owner as key
 
     public MiniTwitter() {
         friendship = new HashMap<>();
@@ -44,11 +44,50 @@ public class MiniTwitter {
      */
     public List<Tweet> getNewsFeed(int user_id) {
         List<Integer> following = friendship.get(user_id);
-        if(following == null) return getTimeline(user_id);
+        if(following == null || following.isEmpty()) return getTimeline(user_id);
 
-        following.add(user_id);
         List<Tweet> result = new ArrayList<>();
 
+        // maxheap for top k tweets
+        PriorityQueue<Tweet> maxHeap = new PriorityQueue<>(new Comparator<Tweet>(){
+            @Override
+            public int compare(Tweet a, Tweet b){
+                return b.id - a.id;
+            }
+        });
+
+        Map<Integer, List<Tweet>> timelineContainer = new HashMap<>();
+        Map<Integer, Integer> indexContainer = new HashMap<>();
+
+        // retrive all following's timeline
+        for(int i : following){
+            List<Tweet> temp = getTimeline(i);
+            if(temp != null && !temp.isEmpty()){
+                timelineContainer.put(i, temp);
+                maxHeap.add(temp.get(0));
+                indexContainer.put(i, 0);
+            }
+        }
+
+        // retrive user's time line
+        List<Tweet> userTimeline = getTimeline(user_id);
+        if(userTimeline != null && !userTimeline.isEmpty()){
+            timelineContainer.put(user_id, userTimeline);
+            maxHeap.add(userTimeline.get(0));
+            indexContainer.put(user_id, 0);
+        }
+
+        int count = 0;
+        while(!maxHeap.isEmpty() && count < 10){
+            Tweet tempTweet = maxHeap.poll();
+            result.add(tempTweet);
+            int index = indexContainer.get(tempTweet.user_id);
+            index++;
+            List<Tweet> tempTimeline = timelineContainer.get(tempTweet.user_id);
+            if(index < tempTimeline.size()) maxHeap.add(tempTimeline.get(index));
+            indexContainer.put(tempTweet.user_id, index);
+            count++;
+        }
 
         return result;
     }
@@ -59,10 +98,12 @@ public class MiniTwitter {
      */
     public List<Tweet> getTimeline(int user_id) {
         List<Tweet> total = tweets.get(user_id);
-        if(total == null || total.size() <= 10) return total;
+        if(total == null) return new ArrayList<Tweet>();
         else{
             List<Tweet> result = new ArrayList<>();
-            for(int i = total.size()-1; i >= 0; i--){
+
+            // retrive tweets from tail to head in order to get sorted tweets by timeline
+            for(int i = total.size()-1; i >= total.size()-10 && i >= 0; i--){
                 result.add(total.get(i));
             }
             return result;
@@ -81,6 +122,7 @@ public class MiniTwitter {
             temp.add(to_user_id);
             friendship.put(from_user_id, temp);
         }
+        System.out.println("follow: " + friendship.get(from_user_id).toString());
     }
 
     /*
@@ -91,5 +133,7 @@ public class MiniTwitter {
     public void unfollow(int from_user_id, int to_user_id) {
         // use Integer to remove specific value
         friendship.get(from_user_id).remove(Integer.valueOf(to_user_id));
+        System.out.println("unfollow: " + friendship.get(from_user_id).toString());
+
     }
 }
